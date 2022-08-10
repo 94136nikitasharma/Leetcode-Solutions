ced1465d3e20a9e9eb75ac6f706402da1c0d5616
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-08-10 09:53:43 +0530

    Time: 40 ms (13.51%), Space: 24 MB (17.10%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -16,16 +16,13 @@ public:
         {
             return NULL;
         }
-        
-        
-           int mid=nums.size()/2;
-            
-        
-         TreeNode*root=new TreeNode(nums[mid]);
+        int mid=nums.size()/2;
+        TreeNode*root=new TreeNode(nums[mid]);
         vector<int>left(nums.begin(),nums.begin()+mid);
         vector<int>right(nums.begin()+mid+1,nums.end());
         root->left=sortedArrayToBST(left);
         root->right=sortedArrayToBST(right);
         return root;
+        
     }
 };
