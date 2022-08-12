ae8a17f203383c5a510ff7a6495441759dfcb722
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-08-12 20:52:19 +0530

    Time: 51 ms (28.30%), Space: 16.2 MB (9.25%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-       int n=graph.size();
-        vector<int>color(n,0);
-        for(int i=0;i<n;i++)
+    bool dfs(int node,vector<int>adj[],vector<int>&color)
+    {
+        for(auto it:adj[node])
         {
-            if(color[i]==0) 
+            if(color[it]==-1)
             {
-            queue<int>q;
-            q.push(i);
-            color[i]=1;
-            while(!q.empty())
-            {
-                int parent=q.front();
-                q.pop();
-                for(auto child:graph[parent])
+                
+                color[it]=1-color[node];
+                if(!dfs(it, adj, color)) 
                 {
-                    if(color[child]==color[parent])
-                    {
-                        return false;
-                    }
-                    else if(color[child]==0)
-                    {
-                        q.push(child);
-                        color[child]=-color[parent];
-                    }
+                return false; 
                 }
             }
-      }
-            
+                else if(color[it] == color[node]) return false; 
         }
-        return true;
+            return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>adj[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int x:graph[i])
+            {
+                adj[i].push_back(x);
+            }
+           
+        }
+        vector<int>color(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                color[i]=1;
+                if(!dfs(i,adj,color))
+                {
+                    return false;
+                }
+                
+            }
+        }
+       return true;
     }
 };
