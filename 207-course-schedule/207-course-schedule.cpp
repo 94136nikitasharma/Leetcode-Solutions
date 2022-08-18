d82083fd3eccd491b5a01919d42b668bfca8a8bb
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-08-18 21:35:25 +0530

    Time: 27 ms (76.31%), Space: 14.4 MB (34.05%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+        
+    
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        
+       vector<int> adj[n];
+        
+        for(auto e:prerequisites){  
+                                     
+            adj[e[1]].push_back(e[0]); 
+        }
+        vector<int> vis(n,0);  
+        vector<int> dfsvis(n,0);
+        
+        for(int i=0;i<n;i++){   
+            
+            if(!vis[i]){
+                
+                if(dfs(i,adj,vis,dfsvis)){  
+                    
+                    return false;
+                }
+            }
+        }
+        return true;  
+        
+        
+    }
+    bool dfs(int node , vector<int> adj[], vector<int> &vis, vector<int> &dfsvis){
+        
+        vis[node] = 1;        
+        dfsvis[node] = 1;
+        
+        for(auto it:adj[node]){
+            
+            if(!vis[it]){              
+                
+                if(dfs(it,adj,vis,dfsvis)){  
+                    return true;
+                }
+            }
+            else if(dfsvis[it]){ 
+                 return true;
+            }
+        }
+        dfsvis[node]=0;   
+        return false;
+    }
+};
