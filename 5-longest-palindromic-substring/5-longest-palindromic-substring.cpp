89d59e43b863d3bea6048932f98bad2b048c5957
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-10-10 09:54:34 +0530

    Time: 224 ms (40.97%), Space: 168.7 MB (18.83%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,33 @@
-class Solution {
-public:
-    string extendmiddle(string s, int i,int j)
-    {
-        while(i>=0 and j<s.length() and s[i]==s[j])
+class Solution
+{
+    public:
+        string extend(string s, int i, int j)
         {
-            i--;
-            j++;
+            while (i >= 0 and j < s.length() and s[i] == s[j])
+            {
+                i--;
+                j++;
+            }
+            return s.substr(i + 1, j - i - 1);
         }
-        return s.substr(i+1,j-i-1);
-    }
-    string longestPalindrome(string s) {
-         int n=s.length();
-        string ans="";
-        for(int i=0;i<n;i++)
+    string longestPalindrome(string s)
+    {
+        int n = s.length();
+        string ans = "";
+        for (int i = 0; i < n; i++)
         {
-            string s1=extendmiddle(s,i,i);
-            string s2=extendmiddle(s,i,i+1);
-            if(s1.length() > ans.length())
+            string s1 = extend(s, i, i);
+            string s2 = extend(s, i, i + 1);
+            if (s1.length() > ans.length())
             {
                 ans = s1;
             }
-            if(s2.length() > ans.length())   
+            if (s2.length() > ans.length())
             {
                 ans = s2;
-             }
+            }
         }
+
         return ans;
     }
 };
