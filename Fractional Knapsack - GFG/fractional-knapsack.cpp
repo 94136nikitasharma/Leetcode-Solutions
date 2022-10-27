3a98dc422a672e37d9ff90fc2e2761f65310b4bc
Nikita Sharma <87696438+94136nikitasharma@users.noreply.github.com>
2022-10-27 10:33:43 +0530

    Added solution - LeetHub

--- Fractional Knapsack - GFG/fractional-knapsack.cpp
+++ Fractional Knapsack - GFG/fractional-knapsack.cpp
@@ -53,55 +53,7 @@ class Solution
     }
         
 };
-/*
-struct Item{
-    int value;
-    int weight;
-};
-*/
 
-
-// class Solution
-// {
-//      private:
-//     //Function to get the maximum total value in the knapsack.
-//   static bool comp(Item a, Item b) {
-//     double r1 = (double)a.value / (double)a.weight;
-//     double r2 = (double)b.value / (double)b.weight;
-//     return r1 > r2; 
-// }
-// // function to return fractionalweights
-// public:
-// double fractionalKnapsack(int W, Item arr[], int n)
-// {
-//     // Your code here
-    
-//     sort(arr, arr + n, comp); 
-    
-//     int curWeight = 0; 
-//     double finalvalue = 0.0; 
- 
-    
-//     for (int i = 0; i < n; i++) {
-       
-//         if (curWeight + arr[i].weight <= W) {
-//             curWeight += arr[i].weight;
-//             finalvalue += arr[i].value;
-//         }
- 
-
-//         else {
-//             int remain = W - curWeight;
-//             finalvalue += (arr[i].value / (double)arr[i].weight) * (double)remain;
-//             break;
-//         }
-//     }
- 
-//     return finalvalue;
-    
-// }
-
-// };
 
 
 
